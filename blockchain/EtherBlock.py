45d2f6bd849a9e8252e3dc6ee6468ef81a193d5c
QiubyZ <41333888+QiubyZ@users.noreply.github.com>
2021-08-11 04:29:00 +0800

    Add files via upload

---
+++ blockchain/EtherBlock.py
@@ -0,0 +1,55 @@
+from libs.getprice import Blockchain
+
+class EtherBlock(Blockchain):
+    def __init__(self, url):
+        super(EtherBlock, self).__init__(url)
+        self.tokenize = {"wallet": url.split("/")[-1], "token": []}
+        self.get_table_token()
+
+    def gettotal(self):
+        return (len(self.tokenize.get("token")))
+
+    def getJson(self):
+        return self.tokenize
+
+    def getWallet(self):
+        return self.tokenize.get("wallet")
+
+    def getToken(self):
+        return self.tokenize.get("token")
+
+    def get_table_token(self):
+        listtoken = self._get_parser().find("ul", {"class": "list list-unstyled mb-0"})
+        if(listtoken):
+            token_list = listtoken.find_all_next("a", {"class":"link-hover d-flex justify-content-between align-items-center"})
+            for i in range(len(token_list)):
+                price = token_list[i].find("span", {"class": "list-usd-value d-block"})
+                if (price):
+                    price = price.text if price.text != "\xa0" else "Tidak ada"
+                else:
+                    price = "Tidak Ada"
+                contract_token = token_list[i]["href"].split("?")[0].replace("/token/", "")
+                token_info = token_list[i].find("span", {"class": "list-amount link-hover__item hash-tag hash-tag--md text-truncate"})
+                if (token_info):
+                    token_value, symbl = token_info.text.split(" ")
+                Token_name = token_list[i].find("span", {"class": "list-name hash-tag text-truncate"}).text
+                format_save = {
+                        "nama": f"{Token_name}",
+                        "symbol": f"{symbl}",
+                        "contract": f"{contract_token}",
+                        "jumlah_token": f"{token_value}",
+                        "usd": f"{price}"
+                }
+                self.tokenize.get("token").append(format_save)
+
+class PolygonScan(EtherBlock):
+    def __init__(self, address_wallet):
+        super(PolygonScan, self).__init__(f"https://polygonscan.com/address/{address_wallet}")
+
+class BinanceSmartChain(EtherBlock):
+    def __init__(self, address_wallet):
+        super(BinanceSmartChain, self).__init__(f"http://bscscan.com/address/{address_wallet}")
+
+class Ethereum(EtherBlock):
+    def __init__(self, address_wallet):
+        super(Ethereum, self).__init__(f"https://etherscan.io/address/{address_wallet}")
