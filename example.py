481d3b5db486d559b6e9717789e5054271b87184
QiubyZ <41333888+QiubyZ@users.noreply.github.com>
2021-08-11 16:03:49 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from blockchain.EtherBlock import BinanceSmartChain, PolygonScan, Ethereum
+from blockchain.EtherBlock import BinanceSmartChain, PolygonScan
 print("Binance SmartChain")
 sc = "0xcE8b739b3f1624D359e5F8E9ea72A826f1d48178"
 bsc = BinanceSmartChain(sc)
@@ -8,8 +8,3 @@ print("Polygon Scan")
 matic = "0xfCbAC176e8b54E6A770F7Ca16AcB51954f9A4D80"
 mitic = PolygonScan(matic)
 print(mitic.getToken())
-
-print("Ether Scan")
-ether = "0xfCbAC176e8b54E6A770F7Ca16AcB51954f9A4D80"
-ether = Ethereum(ether)
-print(ether.getToken())
